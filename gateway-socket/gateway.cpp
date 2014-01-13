45052aebdd8a3f700936f0aecd9711ba2d02d94e
leeopop <dlrmsghd@gmail.com>
2014-01-14 01:31:05 +0900

    add event mapping

--- gateway-socket/gateway.cpp
+++ gateway-socket/gateway.cpp
@@ -107,6 +107,13 @@ Gateway::Gateway(const char* inDev, const char* outDev)
 	struct event *term_event = event_new(evbase, termEventFD,
 			EV_READ, terminate_gateway, evbase);
 	event_add(term_event, NULL);
+
+	struct event *add_static_event = event_new(evbase, addStaticIPEventFD,
+			EV_READ | EV_PERSIST, Gateway::add_static_ip, evbase);
+	event_add(add_static_event, NULL);
+	struct event *del_static_event = event_new(evbase, delStaticIPEventFD,
+			EV_READ | EV_PERSIST, Gateway::del_static_ip, evbase);
+	event_add(del_static_event, NULL);
 }
 
 Gateway::~Gateway()
