e8dc66929063b90fc38ca5be1a088aee933984ff
aon <jwchae8@gmail.com>
2014-01-15 02:52:29 +0900

    Now we can write DHCP response

--- gateway-socket/dhcp.hh
+++ gateway-socket/dhcp.hh
@@ -9,7 +9,9 @@
 #define DHCP_HH_
 
 #include "packet.hh"
+#include <netinet/in.h>
 #include <net/ethernet.h>
+#include <vector.h>
 
 class DHCP
 {
@@ -22,6 +24,7 @@ class DHCP
         char getOpcode();
         uint32_t getTransactionID();
         struct ether_addr getClientMAC();
+        int writeResponse(Packet* packet, uint32_t transaction_id, in_addr client_in_addr, ether_addr client_ether_addr, in_addr server_identifier, in_addr subnet_mask, in_addr router, vector<in_addr> dns_vector);
 };
 
 #endif
